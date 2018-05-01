8f95a18dc06308cde30d0b7842595a7f986dc825
alisonstardust <zakulisami@o2.pl>
2018-05-01 21:12:30 +0200

    rewriting

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+//gonna rewrite it in ES6
+let apiAddress = "https://fcc-weather-api.glitch.me/api/current?";
 
-var apiAddress = "https://fcc-weather-api.glitch.me/api/current?";
-
-$( document ).ready(function(){
+$(document).ready(function(){
   var lat, lon, temp;
    if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
-      var lat = "lat=" + position.coords.latitude;
-      var lon = "lon=" + position.coords.longitude;
+      lat = "lat=" + position.coords.latitude;
+      lon = "lon=" + position.coords.longitude;
       getWeather(lat, lon);
     })
   } else {
@@ -21,13 +21,12 @@ $( document ).ready(function(){
       $("#how").text(result.weather[0].main);
       $("#city").text(result.name + ", ");
       $("#country").text(result.sys.country);
-      var currentTempInCelsius = Math.round(result.main.temp * 10) / 10;
+      let currentTempInCelsius = Math.round(result.main.temp * 10) / 10;
       $("#temp").text(currentTempInCelsius + " " + String.fromCharCode(176));
       $("#tempunit").text(tempUnit);
     }
   });
 }
 
-
-var x = new Date();
+let x = new Date();
 document.getElementById("current").innerHTML = x;
