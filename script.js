36e27c22e9bbbf34d58878dab6571c6ac3a61c28
alisonstardust <zakulisami@o2.pl>
2018-05-08 21:26:02 +0200

    rewritten in ES6

--- script.js
+++ script.js
@@ -1,55 +1,51 @@
-//gonna rewrite it in ES6
-let apiAddress = "https://fcc-weather-api.glitch.me/api/current?";
-const city = document.querySelector(".print-city");
-const country = document.querySelector(".print-country");
-const current = document.querySelector(".print-current");
-const temperature = document.querySelector(".print-temperature");
-const how = document.querySelector(".print-how");
-
 function startapp() {
-  current.innerHTML = new Date();
+
+  let apiAddress = "https://fcc-weather-api.glitch.me/api/current?";
+  const city = document.querySelector(".print-city");
+  const country = document.querySelector(".print-country");
+  const current = document.querySelector(".print-current");
+  const temperature = document.querySelector(".print-temperature");
+  const how = document.querySelector(".print-how");
+
+  let date = new Date();
+  current.innerHTML = `${date.toUTCString()} ⏰`;
   var lat, lon, temp;
-   if (navigator.geolocation) {
+  
+  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
-      lat = "lat=" + position.coords.latitude;
-      lon = "lon=" + position.coords.longitude;
-      console.log(apiAddress);
-      getWeather(lat, lon);
+    lat = "lat=" + position.coords.latitude;
+    lon = "lon=" + position.coords.longitude;
+    getWeather(lat, lon);
     })
   } else {
     alert("Geolocation is not supported by this browser.");
   }
-}
 
-function getWeather(lat, lon) {
-  var urlString = apiAddress + lat + "&" + lon;
-  fetch(urlString)
-    .then(resp => resp.json())
-    .then(getSepecific)
-}
+  function getWeather(lat, lon) {
+    var urlString = apiAddress + lat + "&" + lon;
+    fetch(urlString)
+      .then(resp => resp.json())
+      .then(getSepecific)
+  }
 
-function getSepecific(responseAgain) {
-  console.log(responseAgain, responseAgain.weather[0].main, responseAgain.weather[0].description);
-  city.innerText = `${responseAgain.name}, ${responseAgain.sys.country} 🏡`;
-  temperature.innerText = `${responseAgain.main.temp} 🌡️`;
-  if (responseAgain.weather[0].main === "Clouds") {
-    how.innerText = `${responseAgain.weather[0].main} ☁️`;
-  } else if (responseAgain.weather[0].main === "Rain") {
-    how.innerText = `${responseAgain.weather[0].main} 🌧️`;
-  } else if (responseAgain.weather[0].main === "Sunny") {
-    how.innerText = `${responseAgain.weather[0].main} ☀️`;
-  } else if (responseAgain.weather[0].main === "Sunny") {
-    how.innerText = `${responseAgain.weather[0].main} 🌨️`;
+  function getSepecific(responseAgain) {
+    let currentTempInCelsius = Math.round(responseAgain.main.temp * 10) / 10;
+    console.log(responseAgain);
+    city.innerText = `${responseAgain.name}, ${responseAgain.sys.country} 🏡`;
+    temperature.innerText = `${responseAgain.main.temp} ${String.fromCharCode(176)} C 🌡️`;
+    //print correct weather description with emoji
+    if (responseAgain.weather[0].main === "Clouds") {
+      how.innerText = `${responseAgain.weather[0].main} ☁️`;
+    } else if (responseAgain.weather[0].main === "Rain") {
+      how.innerText = `${responseAgain.weather[0].main} 🌧️`;
+    } else if (responseAgain.weather[0].main === "Sunny") {
+      how.innerText = `${responseAgain.weather[0].main} ☀️`;
+    } else if (responseAgain.weather[0].main === "Snow") {
+      how.innerText = `${responseAgain.weather[0].main} 🌨️`;
+    } else if (responseAgain.weather[0].main === "Sunny") {
+      how.innerText = `${responseAgain.weather[0].main} 🌫️`;
+    }
   }
 }
 
 startapp();
-/*
-
-      let currentTempInCelsius = Math.round(result.main.temp * 10) / 10;
-      $("#temp").text(currentTempInCelsius + " " + String.fromCharCode(176));
-      $("#tempunit").text(tempUnit);
-    }
-  });
-}
-*/
